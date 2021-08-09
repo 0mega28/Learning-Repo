af0e375cc2d129545cb5299db72d5960b0f7d544
Saksham Gupta <sakshamdam54321@gmail.com>
2021-08-09 11:53:04 +0530

    Exit with correct exit status in case of failure
    Also, when a syscall fails in this case if the file does
    not exist, errno global variable is set and the error message
    can be read by calling strerror function with errno as argument

--- simple_cat.c
+++ simple_cat.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
 	if (argc <= 1)
 	{
 		printf("Too few arguments\n");
-		exit(0);
+		exit(EXIT_FAILURE);
 	}
 
 	FILE *file = fopen(argv[1], "r");
 
 	if (!file)
 	{
-		printf("File not found\n");
-		exit(0);
+		printf("%s\n", strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 
 	char c;
